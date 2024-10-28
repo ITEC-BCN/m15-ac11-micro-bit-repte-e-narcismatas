f9aa9e3a9eefa2b6491835037e545e40acdec260
Narcis Matas <146827232+narcismatas@users.noreply.github.com>
2024-10-28 16:31:47 +0100

    Repte E

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 
+"""
+def on_forever():
+    temp = input.temperature()
+    led.plot_bar_graph(temp, 50)
+basic.forever(on_forever)
+"""
+
+x = 2
+y = 2
+
+def on_forever():
+    global x, y
+    led.plot(x, y)
+    pause(50)
+    led.unplot(x, y)
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    if accX < -150 and x > 0:
+        x = x - 1
+    elif accX > 150 and x < 4:
+        x = x + 1
+    if accY < -150 and y > 0:
+        y = y - 1
+    elif accY > 150 and y < 4:
+        y = y + 1
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,29 @@
+/** 
+def on_forever():
+    temp = input.temperature()
+    led.plot_bar_graph(temp, 50)
+basic.forever(on_forever)
 
+ */
+let x = 2
+let y = 2
+basic.forever(function on_forever() {
+    
+    led.plot(x, y)
+    pause(50)
+    led.unplot(x, y)
+    let accX = input.acceleration(Dimension.X)
+    let accY = input.acceleration(Dimension.Y)
+    if (accX < -150 && x > 0) {
+        x = x - 1
+    } else if (accX > 150 && x < 4) {
+        x = x + 1
+    }
+    
+    if (accY < -150 && y > 0) {
+        y = y - 1
+    } else if (accY > 150 && y < 4) {
+        y = y + 1
+    }
+    
+})
